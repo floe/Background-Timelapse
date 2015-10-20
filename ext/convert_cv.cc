ec541bdb8a993ec9b0315cfda142573660c266db
Florian Echtler <floe@butterbrot.org>
2015-10-20 15:33:39 +0200

    add opencv-based converter tool for very old raw images

---
+++ ext/convert_cv.cc
@@ -0,0 +1,33 @@
+// compile with: g++ -Wall -o convert_cv convert_cv.cc -lopencv_core -lopencv_imgproc -lopencv_highgui
+
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+uint8_t buffer[1024*1024];
+uint8_t outbuf[1024*1024];
+
+int main(int argc, char* argv[]) {
+
+  int h = 320;
+  int w = 480;
+
+ 	FILE* fin = fopen( argv[1], "r" );
+	fread( buffer, 1, 230400, fin );
+	fclose(fin);
+
+	Mat mYuv(h + h/2, w, CV_8UC1, buffer);
+  Mat mRgba(h,w,CV_8UC4,outbuf);
+  cvtColor( mYuv, mRgba, CV_YUV2BGR_NV21 );
+
+  imwrite(argv[2],mRgba);
+
+  return 0;
+}
